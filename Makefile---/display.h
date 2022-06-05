0da730fedee7096a92626e4ad5be47b98b8ac34d
Laynt <106876316+Laynt@users.noreply.github.com>
2022-06-06 01:16:24 +0200

    Update display.h

--- Makefile---/display.h
+++ Makefile---/display.h
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <time.h>
-#define COLS 25
-#define ROWS 15
+#define COLS 15
+#define ROWS 25
 
 char pawn(int a);
 void boardDisplay(int board[ROWS][COLS]);
